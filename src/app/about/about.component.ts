7ff70a58c796142e16453ba04d982bdfccaa4f14
Caleb Grams <cmgrams@gmail.com>
2018-12-31 11:02:02 +1100

    about4

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,9 +6,45 @@ import { NgAnimateScrollService } from 'ng-animate-scroll';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+	  animations: [
+	  trigger('changeDivSize', [
+	    state('initial', style({
+	      color: 'white',
+	      background: 'white',
+	      textShadow: '0px 0px 0 #0c0c0c, 0px 0px 0 #0c0c0c, 0px 0px 0 #0c0c0c, 0px 0px 0 #0c0c0c'
+	    })),
+	    state('final', style({
+	      color: '#f4f4f4',
+	      background: '#62b8f7',
+		  textShadow: '-1px -1px 0 #0c0c0c, 1px -1px 0 #0c0c0c, -1px 1px 0 #0c0c0c, 1px 1px 0 #0c0c0c'
+	    })),
+	    transition('initial=>final', animate('400ms')),
+	    transition('final=>initial', animate('300ms'))
+	  ]),
+	]
+
+
 })
 export class AboutComponent implements OnInit {
+
+	currentState1 = 'initial';
+	currentState2 = 'initial';
+	currentState3 = 'initial';
+
+		@HostListener('window:scroll', ['$event'])
+		changeState() {
+				const componentPosition = this.el.nativeElement.offsetTop;
+			    const scrollPosition = window.pageYOffset + 200;
+			    const scrollPosition2 = window.pageYOffset -0;
+			    const scrollPosition3 = window.pageYOffset -60;
+
+		    	if (scrollPosition >= componentPosition) { this.currentState1 = 'final'; } else { this.currentState1 = 'initial';}
+		    	if (scrollPosition2 >= componentPosition) { this.currentState2 = 'final'; } else { this.currentState2 = 'initial';}
+		    	if (scrollPosition3 >= componentPosition) { this.currentState3 = 'final'; } else { this.currentState3 = 'initial';}
+
+ 
+	}
 
 	constructor(public el: ElementRef, private animateScrollService: NgAnimateScrollService){}
 	ngOnInit(){}
